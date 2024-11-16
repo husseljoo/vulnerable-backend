13eabe3071e85209a143090f936c29ffed44fa78
Ubuntu <fred@azure-vm.mwhcog1xnh3exc5fcvkj1j03ia.ax.internal.cloudapp.net>
2024-11-16 14:40:49 +0000

    regiser redirects to login sucessfully

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,16 +58,7 @@ class AuthController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        $token = Auth::login($user);
-        return response()->json([
-            'status' => 'success',
-            'message' => 'User created successfully',
-            'user' => $user,
-            'authorisation' => [
-                'token' => $token,
-                'type' => 'bearer',
-            ]
-        ]);
+        return redirect()->route('login')->with('success', 'Registration successful! Please log in.');
     }
 
     public function logout()
